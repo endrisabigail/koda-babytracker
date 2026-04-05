9df71927cf749a3c317a21dd8c3093bab239e59a
MariaZunigaVega <zunigavegamaria@gmail.com>
2026-04-05 18:03:18 -0500

    Create Diaper model with schema for MongoDB

---
+++ koda-server/model/diaper.js
@@ -0,0 +1,9 @@
+//mdz0019
+const mongoose = require('mongoose');
+
+const DiaperSchema = new mongoose.Schema({
+    type : { type: String, enum: ['Wet', 'Dirty', 'Mixed'], required: true },
+    timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Diaper', DiaperSchema);
